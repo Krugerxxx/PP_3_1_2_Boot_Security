0c14cfa6404ea57d9d0b2150f65c6bd4658f07ff
Krugerxxx <sssnik@mail.ru>
2022-09-30 17:22:42 +0500

    v 2.1.4 (3) For checking, add logout

--- src/main/java/ru/kata/spring/boot_security/demo/controller/LogoutController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/LogoutController.java
@@ -1,13 +1,10 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 
