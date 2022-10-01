1c90d9be2a2e8a4a1c2d372faad7039e7ef63091
Krugerxxx <sssnik@mail.ru>
2022-10-02 03:24:27 +0500

    v 2.2.1 (6) левая панель гуд

--- src/main/java/ru/kata/spring/boot_security/demo/controller/HomeController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/HomeController.java
@@ -1,23 +1,37 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
+    private UserService userService;
+    private RoleService roleService;
+
+    @Autowired
+    public HomeController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
 
     //TODO найти как перенаправить в конфиге security
     @GetMapping(value = "/")
     public String home() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!authentication.isAuthenticated()){
+        if (!authentication.isAuthenticated()) {
             return "redirect:/login";
         } else if (authentication.getAuthorities().stream().anyMatch(n -> n.getAuthority().equals("ADMIN"))) {
             return "redirect:/admin";
@@ -25,6 +39,16 @@ public class HomeController {
             return "redirect:/user";
         }
     }
+
+
+    @GetMapping("/user")
+    public String welcome(@AuthenticationPrincipal UserDetails userDetails, Model model, User user) {
+        model.addAttribute("roleName", roleService.findAll());
+        model.addAttribute("activeUser", userService.findByUsername(userDetails.getUsername()));
+        model.addAttribute("userslist", userService.findAll());
+        return "users/users";
+    }
+
 
 /*    @GetMapping("/logout")
     public String logout(HttpServletRequest request) {
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,7 +5,7 @@ let currentUser = {
         currentUser.user = await fetch('api/user')
             .then(response => response.json())
         updateHrefCurrentUser()
-        //updateUserPage()
+        updateUserPage()
     },
 
     /*update: async () => {
@@ -18,7 +18,7 @@ let currentUser = {
     hasRole: (name) => {
         let result = false
         currentUser.user.roles.forEach(role => {
-            if (role.name === name) {
+            if (role.name == name) {
                 result = true
             }
         })
@@ -42,18 +42,100 @@ async function updateHrefCurrentUser() {
 
 
 //Обновление данных на станице данных текущего пользователя
-/*async function updateUserPage() {
-    let row = $('#userPageTable tbody tr:first')
-    row.find('td').eq(0).text(currentUser.user.id)
-    row.find('td').eq(1).text(currentUser.user.name)
-    row.find('td').eq(2).text(currentUser.user.username)
-    row.find('td').eq(3).text(currentUser.getRoleLabelList().toString())
-    row.find('td').eq(4).text((currentUser.user.enabled) ? 'Yes' : 'No')
+async function updateUserPage() {
+    let value = $('#userPageTable tbody tr:first')
+    value.find('td').eq(0).text(currentUser.user.id)
+    value.find('td').eq(1).text(currentUser.user.firstname)
+    value.find('td').eq(2).text(currentUser.user.lastname)
+    value.find('td').eq(3).text(currentUser.user.age)
+    value.find('td').eq(4).text(currentUser.user.email)
+    value.find('td').eq(5).text(currentUser.getRoles().join(' ').toString())
+}
+
+
+//
+/*
+let users = {
+    userMap: new Map(),
+    update: async () => {
+        let userList = await fetch('/api/admin/users').then(response => response.json())
+        userList.forEach(user => users.userMap.set(user.id, user))
+        updateUserTable()
+    },
+    remove: async (id) => {
+        users.userMap.delete(id)
+        updateUserTable()
+    },
+    save: async (user) => {
+        users.userMap.set(user.id, user)
+        updateUserTable()
+    }
+}
+*/
+
+//Список всех ролей
+/*
+let allRoles = {
+    list: [],
+    update: async () => {
+        allRoles.list = await fetch('/api/admin/roles')
+            .then(response => response.json())
+        await updateRolesInNewUserForm()
+    }
 }*/
+
+
+/*
+async function updateUserTable() {
+    let table = $("#userTable tbody")
+    table.html("")
+    users.userMap.forEach(user => {
+        let button_edit   = '<button class="btn btn-info user-edit-button" data-toggle="modal" data-target="#userEditModal" data-user-id="'+user.id+'">Edit</button> '
+        let button_delete = '<button class="btn btn-danger user-delete-button" data-toggle="modal" data-target="#userDeleteModal" data-user-id="'+user.id+'">Delete</button>'
+        let roles = []
+        user.roles.forEach(role => roles.push(role.label))
+        let row = "<tr>" +
+            "<td>"+user.id+"</td>" +
+            "<td>"+user.name+"</td>" +
+            "<td>"+user.username+"</td>" +
+            "<td>"+roles+"</td>" +
+            "<td>"+((user.enabled) ? "Yes" : "No")+"</td>" +
+            "<td>"+button_edit+" "+button_delete+"</td>" +
+            "</tr>"
+
+        table.append(row)
+    })
+}
+*/
+
+
+
+
+/*
+async function updateRolesInNewUserForm() {
+    let select = $('#newUserForm select')
+    select.html('')
+    allRoles.list.forEach(role => select.append("<option value='"+role.name+"'>"+role.label))
+}
+
+*/
 
 
 
 $(document).ready(async function () {
+
+    let page = location.pathname.substring(1)
+    if (page == 'admin') {
+        $('#home-tab').tab('show')
+    }else {
+        $('#profile-tab').tab('show')
+    }
+    $('#left-menu a').on('click', function (event) {
+        event.preventDefault()
+        window.history.pushState('', '', event.target.href);
+        $(this).tab('show')
+    })
+
 
     //Получение текущего пользователя и сопутствующая ему инициализация
     await currentUser.getUser()
